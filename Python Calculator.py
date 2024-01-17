c2f63c13b1bb93c482918e7ffc2f51bfcd516447
Mattias Andersson <mattias0306@outlook.com>
2024-01-18 00:21:26 +0100

    first commit

---
+++ Python Calculator.py
@@ -0,0 +1,10 @@
+print("Miniräknare");
+print("1: Multplikation");
+print("2: Addition");
+print("3: Subtraktion");
+kalkulationer = input("Skriv numer för vad du vill göra: ")
+kalkulationer1 = int(kalkulationer);
+if kalkulationer1 == 1:
+    print("test")
+else:
+    print("Hello")
